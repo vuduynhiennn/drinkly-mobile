c1be49cb4dde8aa4206f3643921509f10fa71268
vuduynhiennn <vuduynhiencute@gmail.com>
2023-05-23 01:42:10 +0700

    feat: add homescreen

---
+++ styles/customStyle.tsx
@@ -0,0 +1,28 @@
+import globalStyle from "./GlobalStyle";
+
+export const {
+    fs24, 
+    color_625D5D,
+    fw700,
+    fs14,
+    uppercase,
+    bg_5DCCFC,
+    py20,
+    center,
+    color_FFFFFF,
+    flex1,
+    bg_pink,
+    justify_bw,
+    px10,
+    px20,
+    px40,
+    radius10,
+    radius5,
+    pbt40,
+    mbt40,
+    mbt60,
+    text_center,
+    mt90,
+    mt_120,
+    py30
+}  = globalStyle
